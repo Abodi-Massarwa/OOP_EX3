0cf7603f7dd43bfda16aaebe88f0306f1943fad0
Abodi Massarweh <abodi.msarwi@gmail.com>
2021-01-11 18:58:17 +0200

    fixed keyError

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -47,10 +47,10 @@ class DiGraph(GraphInterface):
 
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
         edge = self.GEdge(id1, id2, weight)
-        if id1 not in self.m_edges.keys():
-            self.m_edges[id1]= {}
-        if id2 not in self.m_edges_inverted.keys():
-            self.m_edges_inverted[id2] = {}
+        # if id1 not in self.m_edges.keys():
+        #     self.m_edges[id1]= {}
+        # if id2 not in self.m_edges_inverted.keys():
+        #     self.m_edges_inverted[id2] = {}
 
         self.m_edges_inverted[id2][id1] = edge
         self.m_edges[id1][id2] = edge
@@ -61,6 +61,8 @@ class DiGraph(GraphInterface):
 
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
         self.m_vertices[node_id] = DiGraph.GNode(node_id)
+        self.m_edges[node_id]={}
+        self.m_edges_inverted[node_id]={}
         DiGraph.m_mc += 1
         return True
         pass
