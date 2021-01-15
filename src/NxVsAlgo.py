b189b0953684855788f8bc71609e7aa5e7a30e3f
abodi massarweh <abodi.msarwi@gmail.com>
2021-01-15 20:16:45 +0200

    added compare functions

--- src/NxVsAlgo.py
+++ src/NxVsAlgo.py
@@ -3,6 +3,7 @@ import time
 from unittest import TestCase
 
 import networkx
+import switch as switch
 
 from src.DiGraph import DiGraph
 from src.GraphAlgo import GraphAlgo
@@ -15,12 +16,52 @@ class TestGraphAlgo(TestCase):
         self.graph_algo = GraphAlgo()
 
     def testShortestPath(self):
-        self.graph_algo.load_from_json('../data/A5')
-        start_time = time.time()
-        self.graph_algo.shortest_path(0, 100)
+        for file_num in range(1, 7):
+            graph_path = get_path(file_num)
+            self.graph_algo.load_from_json(graph_path)
+            start_time = time.time()
+            self.graph_algo.shortest_path(0, 9)
+            end_time = time.time()
+            print("")
+            print("Testing shortest path - " + graph_path)
+            print("graph algo: " + str(end_time - start_time))
+            self.nx_from_json(graph_path)
+            start_time = time.time()
+            networkx.dijkstra_path_length(self.nx_graph, 0, 9)
+            end_time = time.time()
+            print("networkX graph: " + str(end_time - start_time))
 
-        end_time = time.time()
-        print(float(end_time - start_time))
+    def testConnectedComponent(self):
+        for file_num in range(1, 7):
+            graph_path = get_path(file_num)
+            self.graph_algo.load_from_json(graph_path)
+            start_time = time.time()
+            self.graph_algo.connected_component(8)
+            end_time = time.time()
+            print("")
+            print("testing Connected Component - " + graph_path)
+            print("graph algo: " + str(end_time - start_time))
+            self.nx_from_json(graph_path)
+            start_time = time.time()
+            self.ConnectedComponent(self.nx_graph, 8)
+            end_time = time.time()
+            print("networkX graph: " + str(end_time - start_time))
+
+    def testConnectedComponent(self):
+        for file_num in range(1, 7):
+            graph_path = get_path(file_num)
+            self.graph_algo.load_from_json(graph_path)
+            start_time = time.time()
+            self.graph_algo.connected_components()
+            end_time = time.time()
+            print("")
+            print("testing Connected Component - " + graph_path)
+            print("graph algo: " + str(end_time - start_time))
+            self.nx_from_json(graph_path)
+            start_time = time.time()
+            networkx.strongly_connected_components
+            end_time = time.time()
+            print("networkX graph: " + str(end_time - start_time))
 
     def nx_from_json(self, file_name):
         with open(f'{file_name}') as f:
@@ -45,3 +86,21 @@ class TestGraphAlgo(TestCase):
             print(ex)
             return False
         return vertex_coordinates
+
+    @staticmethod
+    def ConnectedComponent(gx, id1: int):
+        for c_ls in networkx.strongly_connected_components(gx):
+            if id1 in c_ls:
+                return c_ls
+
+
+def get_path(file_num):
+    switcher = {
+        1: '../data/G_10_80_1.json',
+        2: '../data/G_100_800_1.json',
+        3: '../data/G_1000_8000_1.json',
+        4: '../data/G_10000_80000_1.json',
+        5: '../data/G_20000_160000_1.json',
+        6: '../data/G_30000_240000_1.json',
+    }
+    return switcher.get(file_num, "")
